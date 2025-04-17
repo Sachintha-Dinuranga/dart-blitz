03f2305e7d809237e9cd97a0a18ef29f0e35f005
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-04-17 15:19:04 +0530

    add additional feature #1

--- main.lua
+++ main.lua
@@ -77,6 +77,10 @@ function love.mousepressed( x, y, button, istouch, presses )
             score = score + 1
             target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
             target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)      
+        elseif mouseToTarget > target.radius then
+            if score > 0 then
+                score = score - 1
+            end
         end
     elseif button == 1 and gameState == 1 then
         gameState = 2
