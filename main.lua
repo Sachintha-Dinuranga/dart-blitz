bdcc4b6746259ac044f9ec358164c884586e50a3
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-04-17 09:58:34 +0530

    add target position

--- main.lua
+++ main.lua
@@ -1,1 +1,45 @@
--- main dart file
+-- load, update, draw
+
+-- run immediately when the game loads - declare any variables here
+function love.load()
+   target = {}
+   target.x = 300
+   target.y = 300
+   target.radius = 50
+
+   score = 0
+   timer = 0
+
+   gameFont = love.graphics.newFont(40)
+end
+
+-- game loop (call every frame that game is running) - 60 fps
+function love.update(dt)
+    
+end
+
+
+-- drawing graphic to the screen(anything invole graphic and images)
+function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 0, 0)
+end  
+
+
+function love.mousepressed( x, y, button, istouch, presses )
+    if button == 1 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+        end
+    end
+end
+
+-- get target x, y postition value, radius and mouse x, y position value 
+function distanceBetween(x1, y1, x2, y2)
+   return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
+end
